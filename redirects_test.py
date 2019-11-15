f556fc4e36bad5c03bec0fa1f08cc3de8aead660
vladv-qa <vladv-qa@users.noreply.github.com>
2019-11-15 14:23:09 +0200

    adding additional functionality: - creating folder if isn't exist - print statistics of the testing

--- redirects_test.py
+++ redirects_test.py
@@ -1,7 +1,8 @@
 import requests
 import csv
 from colorama import init, Fore, Style
-
+import traceback
+import os
 init()
 
 
@@ -24,17 +25,27 @@ def read_csv(file_name, domain):
 
 
 def file_write(result, file_name):
+    try:
+        dir_name = 'reports'
+        os.makedirs(dir_name)
+        print("Directory ", dir_name, " Created ")
+    except FileExistsError:
+        pass
     with open(f'reports/{file_name}', 'w', encoding='utf-8', newline='') as file:
         a_pen = csv.writer(file)
         a_pen.writerow(('Base URl', 'Expected URL', 'Current URL', 'Assertation'))
         for row in result:
             a_pen.writerow((row['base_url'], row['expected_url'], row['current_url'], row['matching']))
     print(f"Writing is finished!!")
-    print(f"Location: /reports/{file_name}")
+    absolute_file_path =  os.path.abspath(os.path.join(dir_name)) + '\\' + file_name
+    print(f"Location: {absolute_file_path}")
 
 
 def check_redirects(base_url, expected_url):
     print("Start checking ...")
+    correct_links = 0
+    uncorrect_links = 0
+    same_page = 0
     result = []
     count = 1
     for b_url, exp_url in zip(base_url, expected_url):
@@ -55,6 +66,7 @@ def check_redirects(base_url, expected_url):
             }
             ]
             print(Fore.GREEN + str(count) + ' ' + str(output_true))
+            correct_links += 1
         elif current_url == b_url:
             info = {
                 'matching': "THE SAME PAGE",
@@ -70,6 +82,7 @@ def check_redirects(base_url, expected_url):
                 'base_url': b_url
             }]
             print(Fore.CYAN + str(count) + ' ' + str(output_same))
+            same_page += 1
         else:
             info = {
                 'matching': False,
@@ -79,16 +92,23 @@ def check_redirects(base_url, expected_url):
             }
             result.append(info)
             output_false = [{
-                'matching': "THE SAME PAGE",
+                'matching': "False",
                 'expected_url': exp_url,
                 'current_url': current_url,
                 'base_url': b_url
             }]
             print(Fore.RED + str(count) + ' ' + str(output_false))
-        count +=1
+            uncorrect_links += 1
+        count += 1
     print(Style.RESET_ALL)
     print("Checking finished ...")
-
+    statistics = [{
+        'Quantity of Test URLs': len(base_url),
+        'Correct redirections': correct_links,
+        'Uncorrect redirections': uncorrect_links,
+        'Redirect to the same age': same_page
+    }]
+    print("Statistics: " + str(statistics) + '\n')
     return result
 
 
@@ -102,15 +122,21 @@ def read_config():
     print(params)
     return params
 
+try:
+    print("--- Script is running!!! ---")
+    parameter = read_config()
+    file = parameter[0]
+    save_name = parameter[1]
+    domain = parameter[2]
+    info = read_csv(file, domain)
+    base_url_list = info[0].get('base_urls')
+    print('Quantity of Test URLs: ' + str(len(base_url_list)))
+    expected_url_list = info[-1].get('expected_urls')
+    result = check_redirects(base_url=base_url_list, expected_url=expected_url_list)
+    file_write(result=result, file_name=save_name)
+    input('Press ENTER to exit')
+except Exception as e:
+    print('Error:\n', traceback.format_exc())
+    input('Press ENTER to exit')
 
-print("--- Script is running!!! ---")
 
-parameter = read_config()
-file = parameter[0]
-save_name = parameter[1]
-domain = parameter[2]
-info = read_csv(file, domain)
-base_url_list = info[0].get('base_urls')
-expected_url_list = info[-1].get('expected_urls')
-result = check_redirects(base_url=base_url_list, expected_url=expected_url_list)
-file_write(result=result, file_name=save_name)
